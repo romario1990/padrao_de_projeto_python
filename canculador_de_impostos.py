ae4046b45d7dbec0656dbb1dee72d714ccdfb254
romario1990 <romario.getulio@gmail.com>
2018-05-28 22:59:03 -0300

    First commit

---
+++ canculador_de_impostos.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+from imposto import ISS, ICMS, ICPP, IKCV
+
+class Calculador_de_imposto(object):
+
+    def realizar_calculo(self, orcamento, imposto):
+
+        imposto_calculado = imposto.calcula(orcamento)
+
+        print(imposto_calculado)
+
+if __name__ == '__main__':
+
+    from orcamento import Orcamento, Item
+
+    calculador = Calculador_de_imposto()
+
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('Item - 1', 50))
+    orcamento.adiciona_item(Item('Item - 2', 200))
+    orcamento.adiciona_item(Item('Item - 3', 250))
+
+    print('ISS e ICMS')
+    calculador.realizar_calculo(orcamento, ISS())
+    calculador.realizar_calculo(orcamento, ICMS())
+
+    print('ICPP e IKCV')
+
+    calculador.realizar_calculo(orcamento, ICPP())
+    calculador.realizar_calculo(orcamento, IKCV())
---
+++ orcamento.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+class Orcamento(object):
+    def __init__(self):
+
+        self.__itens = []
+
+    @property
+    def valor(self):
+        total = 0.0
+        for item in self.__itens:
+            total+= item.valor
+        return total
+
+    def obter_itens(self):
+
+        return tuple(self.__itens)
+
+    @property
+    def total_itens(self):
+        return len(self.__itens)
+
+    def adiciona_item(self, item):
+        self.__itens.append(item)
+
+class Item(object):
+
+    def __init__(self, name, valor):
+        self.__name = name
+        self.__valor = valor
+
+    @property
+    def valor(self):
+
+        return self.__valor
+
+    @property
+    def name(self):
+
+        return self.__name
+
